1e04cfcd3b4fad6e29a03cd36442a4ad90cf3584
Max Winde <max.winde@gmail.com>
2011-11-29 16:53:29 +0100

    hey! it works!

---
+++ primaryImageColors.js
@@ -0,0 +1,46 @@
+function imageColors(imageUrl, callback) {
+	var image = new Image();
+	image.onload = function() {
+		var width = Math.round(image.width / 2);
+		var height = Math.round(image.height / 2)
+		var $canvas = $('<canvas>').attr({width: width, height: height});
+		var context = $canvas[0].getContext('2d');
+
+		context.drawImage(image, 0, 0, width, height);
+
+		var imageData = context.getImageData(0, 0, width, height);
+		var colorIndex = {};
+		var colors = [];
+
+		for(var i = 0; i < imageData.width * imageData.height; i++) {
+			var red = imageData.data[i * 4];
+			var green = imageData.data[i * 4 + 1];
+			var blue = imageData.data[i * 4 + 2];
+			var alpha = imageData.data[i * 4 + 3];
+
+			if (alpha < 128) continue;
+
+			var diversity = Math.max(Math.abs(red - green), Math.abs(red - blue));
+
+			var colorId = 'rgba(' + red + ',' + green + ',' + blue + ',' + alpha + ')';
+
+			var colorI = colorIndex[colorId];
+			var color = null;
+			if (colorI) {
+				colors[colorI].pixels++;
+			} else {
+				color = {pixels: 1, rgba: colorId, red: red, green: green, blue: blue, alpha: alpha, diversity: diversity};
+				colors.push(color);
+				colorIndex[colorId] = colors.length - 1;
+			}
+		}
+
+		colors.sort(function(a, b) {
+			//return b.diversity - a.diversity;
+			return b.pixels * b.pixels * b.diversity - a.pixels * a.pixels * a.diversity;
+		});
+
+		callback(colors, imageData.width * imageData.height);
+	}
+	image.src = imageUrl;
+}
